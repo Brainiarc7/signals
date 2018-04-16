0034862be55240cbd505adb4c939947dabec8d80
Dennis E. Mungai <dmngaie@gmail.com>
2018-04-16 19:19:28 +0300

    Pull the latest commits from zenbuild
    The previous preference (which required a gpac patch applied) is no longer needed as zenbuild has been updated upstream.
    See https://github.com/gpac/zenbuild

--- extra.sh
+++ extra.sh
@@ -55,10 +55,10 @@ echo zenbuild
 if [ ! -f extra/src/zenbuild/zenbuild.sh ] ; then
 	mkdir -p extra/src
 	rm -rf extra/src/zenbuild
-	git clone https://github.com/gpac/zenbuild extra/src/zenbuild
+	git clone https://github.com/gpac/zenbuild -b master extra/src/zenbuild
 	pushd extra/src/zenbuild
-	git checkout 20816630da5b3
-	patch -p1 < ../../patches/gpac_01_revision.diff
+	git  pull
+	#patch -p1 < ../../patches/gpac_01_revision.diff
 	#patch -p1 < ../../patches/ffmpeg_01_version.diff
 	popd
 fi
